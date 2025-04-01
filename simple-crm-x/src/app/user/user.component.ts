2ca207563c2e3273bb2f86c2476a63cce612a5e1
Jean Pondy <manbayesse@gmail.com>
2025-04-01 04:57:31 +0200

    add user button (FAB) / Tooltip

--- simple-crm-x/src/app/user/user.component.ts
+++ simple-crm-x/src/app/user/user.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule, TooltipPosition} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  position: TooltipPosition = 'above'; // Define Tooltip Position
 }
