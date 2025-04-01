c667d7a13204f55615497b5d51bbf4a081bf5a77
Jean Pondy <manbayesse@gmail.com>
2025-04-01 15:09:35 +0200

    Textfelder anzeigen/Nutzer infos

--- simple-crm-x/src/app/dialog-add-user/dialog-add-user.component.ts
+++ simple-crm-x/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog'; // Wichtig!
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatButtonModule, FormsModule, MatDialogModule], // Hier hinzufügen!
+  providers: [provideNativeDateAdapter()],
+  imports: [MatButtonModule, FormsModule, MatDialogModule,MatFormFieldModule, MatInputModule, MatDatepickerModule ], // Hier hinzufügen!
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
