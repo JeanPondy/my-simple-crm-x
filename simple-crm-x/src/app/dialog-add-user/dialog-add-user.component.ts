8fc6ec5917f72156274613fa56b2b9ed8495dca1
Jean Pondy <manbayesse@gmail.com>
2025-04-01 17:51:30 +0200

    NgModell - Variable mit Input verknüpfen (new User mit User-Model verknüpfen  )

--- simple-crm-x/src/app/dialog-add-user/dialog-add-user.component.ts
+++ simple-crm-x/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -6,13 +6,23 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {provideNativeDateAdapter} from '@angular/material/core';
+import { User } from '../models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatButtonModule, FormsModule, MatDialogModule,MatFormFieldModule, MatInputModule, MatDatepickerModule ], // Hier hinzufügen!
+  imports: [MatButtonModule, FormsModule, MatDialogModule,MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule ], // Hier hinzufügen!
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
-export class DialogAddUserComponent { }
+export class DialogAddUserComponent { 
+  user = new User();
+ 
+  birthDate: Date = new Date();
+
+  saveUser(){
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('Current User is', this.user);
+  }
+}
