d5eb951ca0c8cdc253be69726d6afa42436e6052
Jean Pondy <manbayesse@gmail.com>
2025-04-02 14:05:10 +0200

    Tabelle im Material-Style: Material Design: card

--- simple-crm-x/src/app/user/user.component.ts
+++ simple-crm-x/src/app/user/user.component.ts
@@ -4,11 +4,12 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule, TooltipPosition} from '@angular/material/tooltip';
 import {MatDialogModule, MatDialog} from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule, MatCardModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
