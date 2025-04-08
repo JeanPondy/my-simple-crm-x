3c464df30ff78f9d52ec5cb669b086ef49d867be
Jean Pondy <manbayesse@gmail.com>
2025-04-08 03:28:25 +0200

    Nutzer mit ID aus Firebase DB abrufen: unser Daten durch echten Daten aus Firebase abrufen

--- simple-crm-x/src/app/user-detail/user-detail.component.ts
+++ simple-crm-x/src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
+import { doc, Firestore, getDoc } from '@angular/fire/firestore';
+import { User } from '../models/user.class';
 @Component({
   selector: 'app-user-detail',
   standalone: true,
@@ -10,14 +12,33 @@ import {MatCardModule} from '@angular/material/card';
 })
 export class UserDetailComponent implements OnInit {
   userId!: string;
+  user: User = new User();
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,  private firestore: Firestore) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => { 
       this.userId = paramMap.get('id') || 'ID nicht gefunden'; 
       console.log('User ID:', this.userId);
+      this.getUser();
     });
+  }
+
+  async getUser() {
+    try {
+      const userDocRef = doc(this.firestore, 'users', this.userId!); // Verweis auf das spezifische Dokument
+      const userSnap = await getDoc(userDocRef); // Dokument auslesen
+      
+      if (userSnap.exists()) {
+          // Type Assertion
+          this.user = new User(userSnap.data() as User); // Daten speichern, wenn das Dokument existiert
+        console.log('Retrieved user:', this.user);
+      } else {
+        console.log('No such document!');
+      }
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
   }
   
 }
